eaa9144e8b866cd8eff5e196aac838eb03a8b0ca
Suryansh singh <22054137@kiit.ac.in>
2026-05-18 22:42:33 +0530

    Refactor robots rules to include disallow directives

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,7 +4,27 @@ import { SITE_URL } from "@/lib/site";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: "*", allow: "/" },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/wp-admin/",
+          "/wp-content/",
+          "/wp-includes/",
+          "/tag/",
+          "/author/",
+          "/feed/",
+          "/comments/",
+          "/*?replytocom=",
+          "/comment.php",
+          "/xmlrpc.php",
+          "/wp-login.php",
+          "/search/",
+        ],
+      },
+    ],
+
     sitemap: `${SITE_URL}/sitemap.xml`,
     host: SITE_URL,
   };
