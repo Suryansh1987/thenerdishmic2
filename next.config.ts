8486129b9aeebd80137f9bc20ccdf5de0a5c1af8
Suryansh singh <22054137@kiit.ac.in>
2026-05-18 22:41:58 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/comment.php",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex",
+          },
+        ],
+      },
+
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "index, follow",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
